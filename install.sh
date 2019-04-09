78a8d99c16fc73ce5cc12ab3651490195a2b3ac3
Gabriele Soldati <gsoldati@capify.co.uk>
2019-04-09 10:22:01 +0100

    added composer installation

--- install.sh
+++ install.sh
@@ -9,8 +9,18 @@ do
  esac 
 done 
 
-# clone laravel app
+# Clone laravel app
 git clone https://github.com/laravel/laravel.git ${DIRECTORY:-laravel}
+
+
+# Mount the directories that you will need for your Laravel project and avoid the overhead of installing Composer globally
+cd ${DIRECTORY:-laravel}
+docker run --rm -v $(pwd):/app composer install
+
+
+
+
+
 
 
 
