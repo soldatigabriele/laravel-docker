650dcec17f031a70d4ae4d3a3327ae5736f42a6f
Gabriele Soldati <gsoldati@capify.co.uk>
2019-04-09 14:12:03 +0100

    script update

--- install.sh
+++ install.sh
@@ -17,25 +17,23 @@ git clone https://github.com/laravel/laravel.git ${DIRECTORY:-laravel}
 cd ${DIRECTORY:-laravel}
 docker run --rm -v $(pwd):/app composer install
 
+# Add the permissions to the user
 cd .. && sudo chown -R $USER ${DIRECTORY:-laravel}
 
-cp Dockerfile ${DIRECTORY:-laravel}/Dockerfile
-cp docker-compose.yml ${DIRECTORY:-laravel}/docker-compose.yml
+# Copy the configuration files to the right folder
+mkdir -p ${DIRECTORY:-laravel}/nginx/conf.d
+mkdir -p ${DIRECTORY:-laravel}/mysql
+mkdir -p ${DIRECTORY:-laravel}/php
 
+cp config/app.conf ${DIRECTORY:-laravel}/nginx/conf.d/app.conf
+cp config/my.cnf ${DIRECTORY:-laravel}/mysql/my.cnf
+cp config/php.ini ${DIRECTORY:-laravel}/php/php.ini
+cp config/env ${DIRECTORY:-laravel}/.env
+cp config/Dockerfile ${DIRECTORY:-laravel}/Dockerfile
+cp config/docker-compose.yml ${DIRECTORY:-laravel}/docker-compose.yml
 
-mkdir -p ${DIRECTORY:-laravel}/nginx/conf.d && cp app.conf ${DIRECTORY:-laravel}/nginx/conf.d/app.conf
-mkdir -p ${DIRECTORY:-laravel}/mysql && cp my.cnf ${DIRECTORY:-laravel}/mysql/my.cnf
-mkdir -p ${DIRECTORY:-laravel}/php && cp php.ini ${DIRECTORY:-laravel}/php/php.ini
-
-cp .env ${DIRECTORY:-laravel}/.env
-
-# Move in the directory again
+# Move in the directory again and run docker-compose up
 cd ${DIRECTORY:-laravel}
 docker-compose up -d
 docker-compose exec app php artisan key:generate
 docker-compose exec app php artisan config:cache
-
-
-
-
-
