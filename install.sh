c46dffa37786864e14068bf1051484121cce89e9
Gabriele Soldati <gsoldati@capify.co.uk>
2019-04-09 14:20:40 +0100

    changed input argument

--- install.sh
+++ install.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
 
-while getopts d:p: option 
-do 
- case "${option}" 
- in 
- d) DIRECTORY=${OPTARG};; 
- p) PASSWORD=${OPTARG};; 
- esac 
-done 
-
 # Clone laravel app
-git clone https://github.com/laravel/laravel.git ${DIRECTORY:-laravel}
-
+git clone https://github.com/laravel/laravel.git ${1:-laravella}
 
 # Mount the directories that you will need for your Laravel project and avoid the overhead of installing Composer globally
-cd ${DIRECTORY:-laravel}
+cd ${1:-laravel}
 docker run --rm -v $(pwd):/app composer install
 
 # Add the permissions to the user
-cd .. && sudo chown -R $USER ${DIRECTORY:-laravel}
+cd .. && sudo chown -R $USER ${1:-laravel}
 
 # Copy the configuration files to the right folder
-mkdir -p ${DIRECTORY:-laravel}/nginx/conf.d
-mkdir -p ${DIRECTORY:-laravel}/mysql
-mkdir -p ${DIRECTORY:-laravel}/php
+mkdir -p ${1:-laravel}/nginx/conf.d
+mkdir -p ${1:-laravel}/mysql
+mkdir -p ${1:-laravel}/php
 
-cp config/app.conf ${DIRECTORY:-laravel}/nginx/conf.d/app.conf
-cp config/my.cnf ${DIRECTORY:-laravel}/mysql/my.cnf
-cp config/php.ini ${DIRECTORY:-laravel}/php/php.ini
-cp config/env ${DIRECTORY:-laravel}/.env
-cp config/Dockerfile ${DIRECTORY:-laravel}/Dockerfile
-cp config/docker-compose.yml ${DIRECTORY:-laravel}/docker-compose.yml
+cp config/app.conf ${1:-laravel}/nginx/conf.d/app.conf
+cp config/my.cnf ${1:-laravel}/mysql/my.cnf
+cp config/php.ini ${1:-laravel}/php/php.ini
+cp config/env ${1:-laravel}/.env
+cp config/Dockerfile ${1:-laravel}/Dockerfile
+cp config/docker-compose.yml ${1:-laravel}/docker-compose.yml
 
 # Move in the directory again and run docker-compose up
-cd ${DIRECTORY:-laravel}
+cd ${1:-laravel}
 docker-compose up -d
 docker-compose exec app php artisan key:generate
 docker-compose exec app php artisan config:cache
